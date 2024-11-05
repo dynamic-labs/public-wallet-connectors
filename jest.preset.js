3627ef11caca406aa002e642c9724c1f9e5afba4
Coffee☕️ <coffee@coffeebot.dev>
2024-11-05 15:04:45 -0500

    fix: update jest.preset

--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,6 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-const esmModules = ['@simplewebauthn'];
+const esmModules = ['@simplewebauthn', "@wagmi", "wagmi"];
 
 module.exports = {
   ...nxPreset,
