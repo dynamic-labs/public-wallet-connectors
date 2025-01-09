a8cb446e9ea6cfa25e2c441aca5c2c3cf3a6b4ef
Arda Erturk <arda.erturk@mail.utoronto.ca>
2025-01-09 09:19:59 -0500

    Update IntersendEvmWalletConnector.ts

--- packages/@dynamic-labs-connectors/intersend-evm/src/IntersendEvmWalletConnector.ts
+++ packages/@dynamic-labs-connectors/intersend-evm/src/IntersendEvmWalletConnector.ts
@@ -69,6 +69,10 @@ export class IntersendEvmWalletConnector extends EthereumInjectedConnector {
     return IntersendSdkClient.getProvider();
   }
 
+  override isInstalledOnBrowser(): boolean {
+    return true;
+  }
+
   override async getAddress(): Promise<string | undefined> {
     return IntersendSdkClient.getAddress();
   }
