55375e00b71715de7f4cdf1bfd179b7f0e80975e
Aditya <46365777+a-d-i-t-y-a-s@users.noreply.github.com>
2025-02-26 21:59:36 +0000

    pushing v1 of solana walletconnect

---
+++ packages/@dynamic-labs-connectors/definitive-solana/src/DefinitiveSolanaWalletConnector.spec.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { SolanaWalletConnectorOpts } from '@dynamic-labs/solana-core';
+import { DefinitiveSolanaWalletConnector } from './DefinitiveSolanaWalletConnector.js';
+
+jest.mock('@dynamic-labs/wallet-connector-core', () => ({
+  ...jest.requireActual('@dynamic-labs/wallet-connector-core'),
+  logger: {
+    debug: jest.fn(),
+  },
+}));
+
+const walletConnectorProps: SolanaWalletConnectorOpts = {
+  walletBook: {} as any,
+  solNetworks: [],
+} as any as SolanaWalletConnectorOpts;
+
+describe('DefinitiveSolanaWalletConnector', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    // Clear any previously defined window.solana.
+    (global as any).window = {};
+  });
+
+  describe('findProvider', () => {
+    it('should return the provider if window.solana is defined', () => {
+      const dummyProvider = {
+        publicKey: { toString: () => 'dummy-address' },
+        connect: jest.fn(),
+        signMessage: jest.fn(),
+      };
+
+      // Simulate an injected Solana provider
+      (global as any).window = { solana: dummyProvider };
+
+      const connector = new DefinitiveSolanaWalletConnector(walletConnectorProps);
+      const provider = connector.findProvider();
+      expect(provider).toBeDefined();
+      expect(provider).toBe(dummyProvider);
+    });
+
+    it('should return undefined if window.solana is not defined', () => {
+      // Ensure window.solana is not defined.
+      (global as any).window = {};
+      const connector = new DefinitiveSolanaWalletConnector(walletConnectorProps);
+      expect(connector.findProvider()).toBeUndefined();
+    });
+  });
+});
