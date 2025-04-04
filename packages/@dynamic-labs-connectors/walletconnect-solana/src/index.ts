12d241e1c908322d7262cb9051f014285ae914bc
Aditya <46365777+a-d-i-t-y-a-s@users.noreply.github.com>
2025-04-04 14:59:26 -0400

    Update index.ts: Removed Commented Code
    Removed Commented Code

--- packages/@dynamic-labs-connectors/walletconnect-solana/src/index.ts
+++ packages/@dynamic-labs-connectors/walletconnect-solana/src/index.ts
@@ -1,20 +1,3 @@
-// import { WalletConnectorConstructor } from "@dynamic-labs/wallet-connector-core";
-// import { WalletConnectSolanaConnector } from './WalletConnectSolanaConnector.js';
-
-// export { WalletConnectSolanaConnector } from './WalletConnectSolanaConnector.js';
-
-
-// export const WalletConnectSolanaConnectors = (
-// //   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars -- we don't care about the props
-// // //   _props: any
-// // // ): WalletConnectorConstructor[] =>
-// // //   !isInIframe() ? [WalletConnectSolanaConnector] : [];
-
-// _props: any
-// ): WalletConnectorConstructor[] => [WalletConnectSolanaConnector];
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 import { WalletConnectSolanaConnector as _WalletConnectSolanaConnector } from "./WalletConnectSolanaConnector.js";
 import { WalletConnectorConstructor } from "@dynamic-labs/wallet-connector-core";
 
